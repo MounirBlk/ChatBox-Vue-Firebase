39c91319aa4cfcd304695544e4d26c38e6de7502
MounirBlk <mounir.ballouk@gmail.com>
2020-11-11 20:01:02 +0100

    resolve

--- src/firebase/initgit.js
+++ src/firebase/initgit.js
@@ -1,15 +1,21 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-      apiKey: "",
-      authDomain: "",
-      databaseURL: "",
-      projectId: "",
-      storageBucket: "",
-      messagingSenderId: "",
-      appId: "",
-      measurementId: ""
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+import firebase from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "h",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings({
+    timestampsInSnapshots: true
+});
+//firebase.analytics();
+
+export default firebaseApp.firestore();
