82db126162176ace7b387d5a22038e24ef71d2da
MounirBlk <mounir.ballouk@gmail.com>
2020-11-11 18:19:45 +0100

    login chat + initiliations et preparation

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
